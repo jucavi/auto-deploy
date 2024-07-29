5db453cf8e5f3260e221cd7a473260614df71dcc
Juan  Carlos Vilarrubia <jvilarrubia@soincon.es>
2024-07-29 09:28:19 +0200

    added: load driver method

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,9 +11,14 @@ module es.emi.automaticdeploy {
     requires org.hibernate.orm.core;
     requires java.persistence;
     requires org.apache.commons.io;
+    requires mysql.connector.j;
+    requires org.postgresql.jdbc;
+    requires com.microsoft.sqlserver.jdbc;
+    requires java.desktop;
 
     opens es.emi.automaticdeploy to javafx.fxml;
     exports es.emi.automaticdeploy;
     exports es.emi.automaticdeploy.controller;
     opens es.emi.automaticdeploy.controller to javafx.fxml;
+    opens es.emi.automaticdeploy.entity to org.hibernate.orm.core;
 }
