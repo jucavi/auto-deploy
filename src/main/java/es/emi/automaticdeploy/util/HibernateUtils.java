38c4b22985f07f98fd02e5d1ccf12246af530679
Juan  Carlos Vilarrubia <jvilarrubia@soincon.es>
2024-07-29 09:29:15 +0200

    fixed: get session factory

--- src/main/java/es/emi/automaticdeploy/util/HibernateUtils.java
+++ src/main/java/es/emi/automaticdeploy/util/HibernateUtils.java
@@ -1,41 +1,52 @@
 package es.emi.automaticdeploy.util;
 
 import es.emi.automaticdeploy.entity.ChangeLog;
+import es.emi.automaticdeploy.entity.ChangeLogLock;
 import es.emi.automaticdeploy.entity.persistence.DatabaseProperties;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtils {
 
-    private final static Map<String, SessionFactory> sessionFactoryMap = new HashMap<>();
+    private static SessionFactory sessionFactory;
 
-    private static SessionFactory buildSessionFactory(DatabaseProperties dbProperties) {
+    public static SessionFactory getSessionFactory(DatabaseProperties dbProperties) {
+        if (sessionFactory == null) {
+            try {
+
+                Configuration configuration = getConfiguration(dbProperties);
+
+                // Annotated classes
+                configuration.addAnnotatedClass(ChangeLog.class);
+                configuration.addAnnotatedClass(ChangeLogLock.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                throw new RuntimeException("There was an error building the factory");
+            }
+        }
+        return sessionFactory;
+    }
+
+    private static Configuration getConfiguration(DatabaseProperties dbProperties) {
 
         Configuration configuration = new Configuration();
+
         configuration.setProperty("hibernate.connection.driver_class", dbProperties.getDriver());
         configuration.setProperty("hibernate.connection.url", dbProperties.getUrl());
         configuration.setProperty("hibernate.connection.username", dbProperties.getUser());
         configuration.setProperty("hibernate.connection.password", dbProperties.getPassword());
         configuration.setProperty("hibernate.dialect", dbProperties.getDialect());
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.format_sql", "true");
+        configuration.setProperty("hibernate.use_sql_comments", "true");
         configuration.setProperty("hibernate.hbm2ddl.auto", "update");
-        configuration.addAnnotatedClass(ChangeLog.class);
 
-        return configuration.buildSessionFactory();
-    }
-
-    public static SessionFactory getSessionFactory(DatabaseProperties dbProperties) {
-
-        String key = dbProperties.getUrl();
-        if (!sessionFactoryMap.containsKey(key)) {
-            sessionFactoryMap.put(key, buildSessionFactory(dbProperties));
-        }
-        return sessionFactoryMap.get(key);
-    }
-
-    public static void shutdown() {
-        sessionFactoryMap.values().forEach(SessionFactory::close);
+        return configuration;
     }
 }
